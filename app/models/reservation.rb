cebd18daf1d74b93c0af23f52dd9e20d28206884
MosDevx <mosesk.wamae@gmail.com>
2023-06-15 16:45:32 +0300

    fix linetrs

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,8 @@
 class Reservation < ApplicationRecord
-
-	validates :start_date, presence: true
+  validates :start_date, presence: true
   validates :end_date, presence: true
   validates :description, presence: true
 
-	belongs_to :user
-	has_many :rooms, through: :room_reservations
-
-
+  belongs_to :user
+  has_many :rooms, through: :room_reservations
 end
