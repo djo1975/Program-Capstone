baa4352e08b82357786d3bbea7267ee3c3c107d1
MosDevx <mosesk.wamae@gmail.com>
2023-06-15 15:31:43 +0300

    remove colums room_id

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,9 @@ class Reservation < ApplicationRecord
 	validates :start_date, presence: true
   validates :end_date, presence: true
   validates :description, presence: true
+
+	belongs_to :user
+	has_many :rooms, through: :room_reservations
+
+
 end
