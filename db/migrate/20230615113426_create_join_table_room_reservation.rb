35014124eeae8d347b0402a43f976d824f13775b
Mohi <MOHISADEGHI302@GMAIL.com>
2023-06-15 15:17:47 +0330

    Fix linters

--- db/migrate/20230615113426_create_join_table_room_reservation.rb
+++ db/migrate/20230615113426_create_join_table_room_reservation.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableRoomReservation < ActiveRecord::Migration[7.0]
   def change
     create_join_table :rooms, :reservations do |t|
-      t.index [:room_id, :reservation_id]
-      t.index [:reservation_id, :room_id]
+      t.index %i[room_id reservation_id]
+      t.index %i[reservation_id room_id]
     end
   end
 end
--- db/migrate/20230615113525_add_details_to_rooms.rb
+++ db/migrate/20230615113525_add_details_to_rooms.rb
@@ -4,11 +4,11 @@ class AddDetailsToRooms < ActiveRecord::Migration[7.0]
     add_column :rooms, :description, :string
     add_column :rooms, :cost_per_day, :float
 
-    unless column_exists?(:reservations, :room_id)
-      add_reference :reservations, :room, foreign_key: true, on_delete: :cascade
-    else
+    if column_exists?(:reservations, :room_id)
       remove_foreign_key :reservations, :rooms
       add_foreign_key :reservations, :rooms, on_delete: :cascade
+    else
+      add_reference :reservations, :room, foreign_key: true, on_delete: :cascade
     end
   end
 end
