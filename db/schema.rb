87861bb8982b4d94417d6e579bb140c73866aa29
Mohi <MOHISADEGHI302@GMAIL.com>
2023-06-16 14:24:56 +0330

    Fix deleting rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_16_073559) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_15_114346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,12 +35,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_073559) do
 
   create_table "reservations", force: :cascade do |t|
     t.bigint "user_id"
+    t.bigint "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "start_date"
     t.date "end_date"
     t.string "description"
-    t.bigint "room_id"
     t.index ["room_id"], name: "index_reservations_on_room_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_16_073559) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "rooms"
+  add_foreign_key "comments", "rooms", on_delete: :cascade
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "comments"
   add_foreign_key "likes", "users"
