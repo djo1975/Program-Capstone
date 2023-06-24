70a0d984dae61f708026e6cdda9db6b0d2fecb02
djo1975 <mladanilic1975@gmail.com>
2023-06-24 15:26:15 +0200

    Liner errors fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-
   protect_from_forgery with: :null_session, if: -> { request.format.json? }
   before_action :authenticate_request, unless: :sign_up_or_sign_in_request?
 
@@ -21,4 +20,3 @@ class ApplicationController < ActionController::Base
     render json: { error: 'Unauthorized' }, status: :unauthorized
   end
 end
-  
