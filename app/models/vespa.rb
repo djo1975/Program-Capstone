227ab36aa70fd2d83dd12919cd400e7331205d8d
djo1975 <mladanilic1975@gmail.com>
2023-06-24 11:59:44 +0200

    Add comments and likes counters

--- app/models/vespa.rb
+++ app/models/vespa.rb
@@ -2,6 +2,10 @@ class Vespa < ApplicationRecord
   has_many :comments
   has_many :reservations
 
+  def comments_count
+    comments.count
+  end
+
   validates :name, presence: true
   validates :icon, presence: true
   validates :cost_per_day, presence: true
